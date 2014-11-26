e5c799a1ea9db6dedcda20b078616e62c03df71b
jjangsangy <jjangsangy@gmail.com>
2014-11-26 11:47:49 -0800

    random formatting

--- kan/__main__.py
+++ kan/__main__.py
@@ -21,27 +21,27 @@ def command_line():
     containing parsed values.
     '''
     description = 'Kan helps you find the book'
-    version = ' '.join([__version__, __release__])
-    parser  = ArgumentParser(prog='kan', description=description)
+    version     = ' '.join([__version__, __release__])
+    parser      = ArgumentParser(prog='kan', description=description)
+
+    subparser   = parser.add_subparsers(help='Search by')
+
+    by_title    = subparser.add_parser('title', help='Book title',)
+    by_title.add_argument('title', help='Book name')
+
+    by_isbn     = subparser.add_parser('isbn', help='ISBN code')
+    by_isbn.add_argument('code', help='Valid ISBN code')
+
+    by_author   = subparser.add_parser('author', help='Book author')
+    by_author.add_argument('author', help='Authors name')
+    by_author.add_argument('--recent', help='Search by most recent', action='store_true')
+    by_author.add_argument('--top', help='Search by best selling', action='store_true')
+
+    # Main Parser
     parser.add_argument(
         '-v', '--version', action='version',
         version="%s v%s" % ('kan', version),
     )
-
-    subparser = parser.add_subparsers(help='Search by')
-
-    bytitle = subparser.add_parser('title', help='Book title',)
-    bytitle.add_argument('title', help='Book name')
-
-    byisbn = subparser.add_parser('isbn', help='ISBN code')
-    byisbn.add_argument('code', help='Valid ISBN code')
-
-    byauthor = subparser.add_parser('author', help='Book author')
-    byauthor.add_argument('author', help='Authors name')
-    byauthor.add_argument('--recent', help='Search by most recent', action='store_true')
-    byauthor.add_argument('--top', help='Search by best selling', action='store_true')
-
-    # Main Parser
     parser.add_argument(
         '--title',
         help='Title of the book',
@@ -98,7 +98,7 @@ def main():
     )
 
     if args.verbose:
-        print('Request URL:\n',query.url, '\n')
+        print('Request URL:\n', query.url, '\n')
 
     # Temporary Display Interface
     books = query.json.get('items', None)
@@ -108,12 +108,13 @@ def main():
     for book in books:
         info = book['volumeInfo']
 
+        # Retrieve ISBN/Reference Info
         if 'industryIdentifiers' in info:
             identifiers = dict([(ref['type'], ref['identifier']) for ref in info['industryIdentifiers']])
 
+            # Prefer ISBN 13 over ISBN 10
             if 'ISBN_13' in identifiers:
                 isbn_id, isbn = 'ISBN_13', identifiers['ISBN_13']
-
             else:
                 isbn_id, isbn = identifiers.popitem()
 
@@ -127,4 +128,5 @@ def main():
             isbn_id=isbn_id,
             isbn=isbn,
         )
+
         print(display)
